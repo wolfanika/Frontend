9569ff554e67072b5338bc32edd752fadf338d30
wolfanika <46903881+wolfanika@users.noreply.github.com>
2023-01-22 18:05:15 +0600

    Add files via upload
    January Java Script Practices

---
+++ js mini projects/p1-p7/p.js
@@ -0,0 +1,10 @@
+// let button = document.getElementById('bg-cc');
+// let body = document.getElementById('bod');
+// const colors = ['green','blue','red'];
+// button.addEventListener('click', bgColorchange);
+//  function bgColorchange(){
+//     body.style.backgroundColor = colors[index];
+//     index = index +1;
+//  };
+    
+
